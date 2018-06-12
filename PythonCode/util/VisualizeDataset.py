ae22e948258d95c14288c18d80a6f3611bb3e97b
zvone <zvonimir.dujmic@hotmail.com>
2018-06-12 11:58:44 +0200

    c7

--- PythonCode/util/VisualizeDataset.py
+++ PythonCode/util/VisualizeDataset.py
@@ -22,7 +22,7 @@ class VisualizeDataset:
     # among multiple attributes (e.g. label which occurs as labelWalking, etc). In such a case they are plotted
     # in the same graph. The display should express whether points or a line should be plotted.
     # Match can be 'exact' or 'like'. Display can be 'points' or 'line'.
-    def plot_dataset(self, data_table, columns, match='like', display='line'):
+    def plot_dataset(self, data_table, columns, match='like', display='line', fileName = False):
         names = list(data_table.columns)
 
         # Create subplots if more columns are specified.
@@ -68,15 +68,23 @@ class VisualizeDataset:
         # Make sure we get a nice figure with only a single x-axis and labels there.
         plot.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
         plot.xlabel('time')
-        plot.show()
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
-    def plot_dataset_boxplot(self, dataset, cols):
+    def plot_dataset_boxplot(self, dataset, cols, fileName = False):
         ax = dataset[cols].plot.box()
         ax.set_ylim(-30,30)
-        plot.show()
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     # This function plots the real and imaginary amplitudes of the frequencies found in the Fourier transformation.
-    def plot_fourier_amplitudes(self, freq, ampl_real, ampl_imag):
+    def plot_fourier_amplitudes(self, freq, ampl_real, ampl_imag, fileName = False):
         plot.hold(True)
         plot.xlabel('Freq(Hz)')
         plot.ylabel('amplitude')
@@ -84,11 +92,15 @@ class VisualizeDataset:
         plot.plot(freq, ampl_real, '+', freq, ampl_imag,'+')
         plot.legend(['real', 'imaginary'], numpoints=1)
         plot.hold(False)
-        plot.show()
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     # Plot outliers in case of a binary outlier score. Here, the col specifies the real data
     # column and outlier_col the columns with a binary value (outlier or not)
-    def plot_binary_outliers(self, data_table, col, outlier_col):
+    def plot_binary_outliers(self, data_table, col, outlier_col, fileName = False):
         data_table = data_table.dropna(axis=0, subset=[col, outlier_col])
         data_table[outlier_col] = data_table[outlier_col].astype('bool')
         f, xar = plot.subplots()
@@ -102,7 +114,11 @@ class VisualizeDataset:
         xar.plot(data_table.index[~data_table[outlier_col]], data_table[col][~data_table[outlier_col]], 'b+')
         plot.legend(['outlier ' + col, 'no outlier' + col], numpoints=1, fontsize='xx-small', loc='upper center',  ncol=2, fancybox=True, shadow=True)
         plot.hold(False)
-        plot.show()
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     # Plot values that have been imputed using one of our imputation approaches. Here, values expresses the
     # 1 to n datasets that have resulted from value imputation.
@@ -137,6 +153,7 @@ class VisualizeDataset:
         plot.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
         plot.xlabel('time')
         plot.hold(False)
+        plot.tight_layout()
         plot.show()
 
     # This function plots clusters that result from the application of a clustering algorithm
@@ -144,7 +161,7 @@ class VisualizeDataset:
     # by means of different types of points. We assume that three data columns are clustered
     # that do not include the label. We assume the labels to be represented by 1 or more binary
     # columns.
-    def plot_clusters_3d(self, data_table, data_cols, cluster_col, label_cols):
+    def plot_clusters_3d(self, data_table, data_cols, cluster_col, label_cols, fileName = False):
 
         color_index = 0
         point_displays = ['+', 'x', '*', 'd', 'o', 's', '<', '>']
@@ -184,12 +201,16 @@ class VisualizeDataset:
 
         plot.legend(handles, labels, fontsize='xx-small', numpoints=1)
         plot.hold(False)
-        plot.show()
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     # This function plots the silhouettes of the different clusters that have been identified. It plots the
     # silhouette of the individual datapoints per cluster to allow studying the clusters internally as well.
     # For this, a column expressing the silhouette for each datapoint is assumed.
-    def plot_silhouette(self, data_table, cluster_col, silhouette_col):
+    def plot_silhouette(self, data_table, cluster_col, silhouette_col, fileName = False):
         # Taken from the examples of scikit learn
         #(http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html)
 
@@ -229,11 +250,15 @@ class VisualizeDataset:
 
         ax1.set_yticks([])  # Clear the yaxis labels / ticks
         ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-        plot.show()
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     # Plot a dendorgram for hierarchical clustering. It assumes that the linkage as
     # used in sk learn is passed as an argument as well.
-    def plot_dendrogram(self, dataset, linkage):
+    def plot_dendrogram(self, dataset, linkage, fileName = False):
         sys.setrecursionlimit(40000)
         plot.title('Hierarchical Clustering Dendrogram')
         plot.xlabel('time points')
@@ -241,11 +266,15 @@ class VisualizeDataset:
         times = dataset.index.strftime('%H:%M:%S')
         #dendrogram(linkage,truncate_mode='lastp',p=10, show_leaf_counts=True, leaf_rotation=90.,leaf_font_size=12.,show_contracted=True, labels=times)
         dendrogram(linkage,truncate_mode='lastp',p=16, show_leaf_counts=True, leaf_rotation=45.,leaf_font_size=8.,show_contracted=True, labels=times)
-        plot.show()
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     # Plot the confusion matrix that has been derived in the evaluation metrics. Classes expresses the labels
     # for the matrix. We can normalize or show the raw counts. Of course this applies to classification problems.
-    def plot_confusion_matrix(self, cm, classes, normalize=False):
+    def plot_confusion_matrix(self, cm, classes, normalize=False, fileName = False):
         # Taken from http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
 
         # Select the colormap.
@@ -267,12 +296,15 @@ class VisualizeDataset:
         plot.tight_layout()
         plot.ylabel('True label')
         plot.xlabel('Predicted label')
-        plot.show()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     # This function plots the predictions or an algorithms (both for the training and test set) versus the real values for
     # a regression problem. It assumes only a single value to be predicted over a number of cases. The variables identified
     # with reg_ are the predictions.
-    def plot_numerical_prediction_versus_real(self, train_time, train_y, regr_train_y, test_time, test_y, regr_test_y, label):
+    def plot_numerical_prediction_versus_real(self, train_time, train_y, regr_train_y, test_time, test_y, regr_test_y, label, fileName = False):
         self.legends = {}
         plot.title('Performance of model for ' + str(label))
 
@@ -304,12 +336,16 @@ class VisualizeDataset:
         plot.annotate('', xy=(test_time[0], y_coord_labels), xycoords='data', xytext=(test_time[-1], y_coord_labels), textcoords='data', arrowprops={'arrowstyle': '<->'})
         plot.annotate('test set', xy=(test_time[int(float(len(test_time))/2)], y_coord_labels*1.02), color='red', xycoords='data', ha='center')
         plot.hold(False)
-        plot.show()
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     # Plot the Pareto front for multi objective optimization problems (for the dynamical systems stuff). We consider the
     # raw output of the MO dynamical systems approach, which includes rows with the fitness and predictions for the training
     # and test set. We select the fitness and plot them in a graph. Note that the plot only considers the first two dimensions.
-    def plot_pareto_front(self, dynsys_output):
+    def plot_pareto_front(self, dynsys_output, fileName = False):
         fit_1_train = []
         fit_2_train = []
         fit_1_test = []
@@ -323,7 +359,11 @@ class VisualizeDataset:
         plot.xlabel('mse on ' + str(dynsys_output[0][0].columns[0]))
         plot.ylabel('mse on ' + str(dynsys_output[0][0].columns[0]))
         #plt.savefig('{0} Example ({1}).pdf'.format(ea.__class__.__name__, problem.__class__.__name__), format='pdf')
-        plot.show()
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     # Plot a prediction for a regression model in case it concerns a multi-objective dynamical systems model. Here, we plot
     # the individual specified. Again, the complete output of the MO approach is used as argument.
@@ -337,7 +377,7 @@ class VisualizeDataset:
     # Visualizes the performance of different algorithms over different feature sets. Assumes the scores to contain
     # a score on the training set followed by an sd, and the same for the test set.
 
-    def plot_performances(self, algs, feature_subset_names, scores_over_all_algs, ylim, std_mult, y_name):
+    def plot_performances(self, algs, feature_subset_names, scores_over_all_algs, ylim, std_mult, y_name, fileName = False):
 
         plot.hold(True)
         width = float(1)/(len(feature_subset_names)+1)
@@ -355,8 +395,12 @@ class VisualizeDataset:
         if not ylim is None:
             plot.ylim(ylim)
         # plot.tight_layout()
-        plot.savefig('perf_overview.png')
-        plot.show()
+        # plot.savefig('perf_overview.png')
+        plot.tight_layout()
+        if fileName:
+            plot.savefig(fileName, dpi=160)
+        else:
+            plot.show()
 
     def plot_performances_classification(self, algs, feature_subset_names, scores_over_all_algs):
         self.plot_performances(algs, feature_subset_names, scores_over_all_algs, [0.70, 1.0], 2, 'Accuracy')
